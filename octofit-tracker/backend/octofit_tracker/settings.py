64a60e53c901972811485ec62bc47744a91d23f7
bhavin1976 <bhavin.n.desai@accenture.com>
2026-03-05 20:54:22 +0000

    fixed all issues

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-61=hj-w-&bvd&n0%8*253xaoa$zngb#x95oi#y#3visy^hdmup
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if os.environ.get('CODESPACE_NAME'):
+    ALLOWED_HOSTS.append(f"{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev")
 
 
 # Application definition
